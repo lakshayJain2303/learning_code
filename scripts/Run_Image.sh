a287100abf8c0f4563c01bb3ec2ed2c94e587741
root <root@ip-172-31-36-55.ap-south-1.compute.internal>
2021-01-16 08:34:39 +0000

    Changing run script

--- scripts/Run_Image.sh
+++ scripts/Run_Image.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-docker run -itd -p 80:80 lakshayjain/hello-world:$1
+check=$( docker ps -q demo_app )
+
+if [[ -n "$check" ]]; then
+   docker run -itd --name demo_app -p 80:80 lakshayjain/hello-world:$1
+else
+   docker rm -f demo_app
+   docker run -itd --name demo_app -p 80:80 lakshayjain/hello-world:$1
+fi
